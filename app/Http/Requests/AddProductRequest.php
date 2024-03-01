9062c968cc41fb1a82d3eaceaa3eefa88017b159
cornel-abang <shupel16@gmail.com.com>
2024-03-01 10:37:30 +0100

    git add .

--- app/Http/Requests/AddProductRequest.php
+++ app/Http/Requests/AddProductRequest.php
@@ -14,11 +14,11 @@ class AddProductRequest extends BaseRequest
         return [
             'title' => 'required|string',
             'price' => 'required|integer',
-            'category_id' => 'required|integer|exists:categories,id',
+            'category_id' => 'required|string|exists:categories,id',
             'description' => 'required|string',
             'gender' => 'required|string|in:male,female,unisex',
             'primary_image' => 'required|string',
-            'avail_qty' => 'required|integer',
+            'avail_qty' => 'required|string',
             'other_images.*' => 'string',
             'colors.*' => 'required|string',
             'sizes.*' => 'required|string',
