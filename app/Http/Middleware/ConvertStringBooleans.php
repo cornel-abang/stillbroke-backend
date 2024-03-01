3bb86fe50c625a50e7d99a90ab86756ac3ed0074
cornel-abang <shupel16@gmail.com.com>
2024-03-01 12:45:12 +0100

    allow string for category_id and avail_qty

--- app/Http/Middleware/ConvertStringBooleans.php
+++ app/Http/Middleware/ConvertStringBooleans.php
@@ -8,6 +8,16 @@ class ConvertStringBooleans extends TransformsRequest
 {
     protected function transform($key, $value)
     {
+        /**
+         * Intentionally done by request from the FE
+         */
+        if ($key === 'category_id' || $key === 'avail_qty') {
+            
+            $cleanedValue = $this->removeDuplicateQuotes($value);
+
+            return $cleanedValue;
+        }
+
         if($value === 'true' || $value === 'TRUE')
             return true;
 
@@ -15,5 +25,10 @@ class ConvertStringBooleans extends TransformsRequest
             return false;
 
         return $value;
+    }
+
+    private function removeDuplicateQuotes(string $string)
+    {
+        return trim($string, '"\'');
     }
 }
