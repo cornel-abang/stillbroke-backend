97bb14fece1a3919171c5468b233821408a65094
cornel-abang <shupel16@gmail.com.com>
2024-03-24 19:08:22 +0100

    smaller fix

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -12,13 +12,13 @@ class UpdateProductRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            'title' => 'required_without_all:price,category_id,description,gender,primary_image,avail_qty|string',
-            'price' => 'required_without_all:title,category_id,description,gender,primary_image,avail_qty|integer',
-            'category_id' => 'required_without_all:price,title,description,gender,primary_image,avail_qty|integer|exists:categories,id',
-            'description' => 'required_without_all:price,category_id,title,gender,primary_image,avail_qty|string',
-            'gender' => 'required_without_all:price,category_id,description,title,primary_image,avail_qty|string|in:male,female,unisex',
-            'primary_image' => 'required_without_all:price,category_id,description,gender,title,avail_qty|string',
-            'avail_qty' => 'required_without_all:price,category_id,description,gender,title|integer',
+            'title' => 'nullable|string',
+            'price' => 'nullable|integer',
+            'category_id' => 'nullable|integer|exists:categories,id',
+            'description' => 'nullable|string',
+            'gender' => 'nullable|string|in:male,female,unisex',
+            'primary_image' => 'nullable|string',
+            'avail_qty' => 'nullable|integer',
             'other_images' => 'nullable|array',
             'extras' => 'nullable|array',
         ];
